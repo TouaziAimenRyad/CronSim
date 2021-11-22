8bd63a37ba1ddf524f3f9d99f3a0371af380b924
Your Name <you@example.com>
2021-11-22 23:53:10 +0100

    request list done and some basic staurned interpretation added there is one problem with opcode it s inversed dont know why

--- src/lib_client.c
+++ src/lib_client.c
@@ -6,15 +6,11 @@
 
 void client_request_list_tasks(uint16_t opcode ){
     int fd;
-    // FIFO file path
-    
     char * myfifo = "./requetes";
     fd=open(myfifo,O_WRONLY);
-    char * req=malloc(sizeof(uint16_t));
-    sprintf(req,"%u",opcode);
-    write(fd,req,sizeof(uint16_t));
+    write(fd,&opcode,sizeof(opcode));
     close(fd);
-    
+
 
 }
 
