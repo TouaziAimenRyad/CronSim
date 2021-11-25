4ffa8b71dc116a76cb5961d6ef2d9e17fa6fa716
= <touazi.aimen@gmail.com>
2021-11-25 16:25:51 +0100

    match saturnd.c just for the merge

--- src/saturnd.c
+++ src/saturnd.c
@@ -4,7 +4,9 @@
 //TODO: make functions in lib_satrund.c for the treatment of req and replying with res  
 int main(int argc, char * argv[]) {
     
-  char *req=malloc(sizeof(uint16_t));
+  
+    char *req_opcode=malloc(sizeof(uint16_t));
+    uint64_t req_task_id;
     char* pathname_req="./requetes";
     char* pathname_res="./reponse";
     if (mkfifo(pathname_req,0777)==-1)
@@ -25,11 +27,12 @@ int main(int argc, char * argv[]) {
      while(1){
     
         int fd_req=open(pathname_req,O_RDONLY);
-        read(fd_req,req,sizeof(uint16_t));
+        read(fd_req,req_opcode,sizeof(uint16_t));
+        read(fd_req,&req_task_id,sizeof(uint64_t));
         close(fd_req);
         //write(1,req,sizeof(uint16_t));
-        printf("opcode = %s \n",req);
+        printf("opcode = %s \n",req_opcode);
+        printf("taskid = %lu \n",req_task_id);
 
-     }
 
 }
