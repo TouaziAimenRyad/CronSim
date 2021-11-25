513364bfc3a3725549223d25e981c355c5827103
Asma Mokeddes <asma.mokeddes@outlook.fr>
2021-11-26 00:55:16 +0100

    First version of out and se

--- include/lib_client.h
+++ include/lib_client.h
@@ -1,7 +1,6 @@
 #include "../include/cassini.h"
-#ifndef LIB_H_ //if you change the file name change the guards 
+#ifndef LIB_H_ //if you change the file name change the guards
 #define LIB_H_
-
 
 void client_request_list_tasks(uint16_t opcode);
 
@@ -9,19 +8,12 @@ void client_request_list_tasks(uint16_t opcode);
 
 void client_req_creat_task(uint16_t opcode);
 
-void client_req_remove_task(uint16_t opcode,uint64_t task_id);
+void client_req_remove_task(uint16_t opcode, uint64_t task_id);
 
 void client_request_terminate(uint16_t opcode);
 
-void client_request_get_times_and_exitcodes(uint16_t opcode,uint64_t taskid);
+void client_request_get_times_and_exitcodes(uint16_t opcode, uint64_t taskid);
 
-
-
-
-
-
-
-
-
+void client_request_get_stdout(uint16_t opcode, uint64_t taskid);
 
 #endif
