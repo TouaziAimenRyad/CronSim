cce0da691e908ba265f58aad30eae19db370c5f3
TOUAZI Aimen Ryad <touazi.aimen@gmail.com>
2021-12-23 21:54:06 +0100

    write res for creat abd remove added

--- src/lib_deamon.c
+++ src/lib_deamon.c
@@ -3,7 +3,66 @@
 
 
 //response functions  
+void deamon_write_res_create(uint64_t taskid)
+{
+   char *pathname_res = "./run/pipes/saturnd-reply-pipe";
+   int fd_res = open(pathname_res, O_RDONLY);
+   if(fd_res==-1)
+    {
+       
+        perror("failed opening of request pipe");
 
+       
+        exit(EXIT_FAILURE);
+    }
+
+    uint16_t reply_code=be16toh(SERVER_REPLY_OK);
+    void * reply=malloc(sizeof(uint64_t)+sizeof(uint16_t));
+    *((uint16_t *)reply)=reply_code;
+    *((uint64_t *)(reply+16))=taskid;
+    write(fd_res,&reply,sizeof(uint64_t)+sizeof(uint16_t));
+
+
+
+}
+
+void deamon_write_res_remove(uint16_t reply_code) //reply code depends on the execution of the server it could be err or ok check server_reply.h
+{
+   char *pathname_res = "./run/pipes/saturnd-reply-pipe";
+   int fd_res = open(pathname_res, O_RDONLY);
+   if(fd_res==-1)
+    {
+       
+        perror("failed opening of request pipe");
+
+       
+        exit(EXIT_FAILURE);
+    }
+    if(reply_code==SERVER_REPLY_OK)
+    {
+       uint16_t reply_ok=be16toh(reply_code);
+       write(fd_res,&reply_ok,sizeof(uint16_t));
+
+    }
+    else
+    {
+       uint16_t reply_err=be16toh(reply_code);
+       uint16_t err_code=be16toh(reply_code);
+       void *reply=malloc(2*sizeof(uint16_t));
+       *((uint16_t *)reply)=reply_err;
+       *((uint16_t *)(reply+16))=err_code;
+       write(fd_res,&reply,sizeof(uint16_t));
+
+    }
+    
+
+
+}
+
+void deamon_write_res_list()
+{
+
+}
 
 
 
