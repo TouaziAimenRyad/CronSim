3caf3d78b52033fcdfad1bdbf0e039500702da92
Asma Mokeddes <asma.mokeddes@outlook.fr>
2022-01-11 19:40:19 +0100

    reply demon

--- include/lib_deamon.h
+++ include/lib_deamon.h
@@ -2,29 +2,42 @@
 #ifndef LIB_D_H
 #define LIB_D_H
 
-//aux functions 
+// Fonctions auxilliaires :  
 
 void save_tasks( struct TASK  *task_table ,uint32_t nbtask);
+
 void read_saved_tasks(struct TASK  *task_table ,int *nbtask);
+
 int checktime(int current_time ,char* time_str);
+
 void execute_task(char * argv);
 
-//response headers
+// Response headers
+
 void deamon_write_res_create(int fd_res,uint64_t taskid);
+
 void deamon_write_res_remove(int fd_res,uint16_t reply_code);
+
 void deamon_write_res_list(int fd_res,struct TASK  *task_table ,uint32_t nbtask);
 
+void demon_write_res_stderr(int fd_res, uint16_t reponse_code, uint32_t output, uint16_t erreur_code);
+
+void demon_write_res_stdout(int fd_res, uint16_t reponse_code, uint32_t output, uint16_t erreur_code);
 
 
 
-//request headers
+
+// Request headers
 
 uint16_t deamon_read_req_opcode(int fd_req);
 
 void deamon_read_req_creat_task(int fd_req,int fd_res,uint64_t taskid , struct TASK  *task_table ,int *nbtask);
 
 void deamon_read_req_remove_task(int fd_req,int fd_res, struct TASK  *task_table,int* nbtask);
-void demon_read_request_stderr_task(int fd);
+
+void demon_read_request_stdout_task(int fd_req,int fd_res);
+
+void demon_read_request_stderr_task(int fd_req,int fd_res);
 
 
 
